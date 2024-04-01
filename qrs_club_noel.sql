506af22d12d848b1438808ba73eab1ee3db627c7
Kevin Muriel <kevinjhoel@hotmail.com>
2024-04-01 07:53:33 -0500

    1-04-2024

--- qrs_club_noel.sql
+++ qrs_club_noel.sql
@@ -1,1 +1,17 @@
 SELECT 
+cd.transaccion,
+cd.tarifario_id,
+cd.cargo,
+cd.precio,
+cd.consecutivo,
+cd.cargo_cups,
+dbd.consecutivo,
+dbd.codigo_producto,
+dbd.total_costo,
+ip.descripcion
+FROM 
+cuentas_detalle cd
+JOIN bodegas_documentos_d dbd ON cd.consecutivo = dbd.consecutivo
+JOIN inventarios_productos ip ON dbd.codigo_producto = ip.codigo_producto
+WHERE
+cd.numerodecuenta = 2039232
