3c48cbece4298f1e812bf2721f465a5a70acf6fd
Kevin Muriel <kevinjhoel@hotmail.com>
2024-04-02 07:40:33 -0500

    2-04-2024

---
+++ tickets_llamadas.sql
@@ -0,0 +1,56 @@
+
+MIGUEL CONSULTA EXTERNA
+MIGUEL.PALACIOS
+EXT 1300
+MODULO DE AGENDA MEDICA PARA QUE LE APARESCA LAS AGENDAS DEL DOCTOR QUE ESTA EN TURNO
+-------------------------------------
+ana maria de ortopedia
+problema con la medica 
+ti 1112049119
+ext 1191
+192.168.6.50
+Gestionado, esperando respuesta
+---------------------------------------
+glpi matrimonio entre 
+odontopedriatria y odontologia general
+ext 1210
+diana barbosa
+esperando a sayli para revisarlo
+---------------------------------------
+
+---------------------------------------
+192.168.80.115
+---------
+192.168.3.58
+7YzyWB7u%1aiDpo$*
+
+(postgres7): INSERT INTO inv_solicitudes_devolucion ( empresa_id, centro_utilidad, documento, bodega, fecha, observacion, usuario_id, fecha_registro, estacion_id, estado, ingreso, parametro_devolucion_id ) VALUES ( '01', '01', 426920, 'FA', '2024-04-01', 'SALIDA', 2028, now(), 'ERA2', '0', 1996739, '1' );INSERT INTO inv_solicitudes_devolucion_d ( documento, codigo_producto, cantidad, documentos_despachos, codigo_producto_sol, fecha_vencimiento, lote ) VALUES ( 426920, '0102010377', 1, '3052751', '0102010377', '2025-02-28', 'OTXID22002A' );INSERT INTO inv_solicitudes_devolucion_d ( documento, codigo_producto, cantidad, documentos_despachos, codigo_producto_sol, fecha_vencimiento, lote ) VALUES ( 426920, '0102011152', 1, '3051032,3051029', '0102011152', '2025-10-27', 'A231083' );INSERT INTO inv_solicitudes_devolucion_d ( documento, codigo_producto, cantidad, documentos_despachos, codigo_producto_sol, fecha_vencimiento, lote ) VALUES ( 426920, '0102011152', 1, '3051032,3051029', '0102011152', '2025-11-28', 'A231206' );INSERT INTO inv_solicitudes_devolucion_d ( documento, codigo_producto, cantidad, documentos_despachos, codigo_producto_sol, fecha_vencimiento, lote ) VALUES ( 426920, '0102011152', 1, '3051032,3051029', '0102011152', '2025-11-27', 'A231207' );INSERT INTO inv_solicitudes_devolucion_d ( documento, codigo_producto, cantidad, documentos_despachos, codigo_producto_sol, fecha_vencimiento, lote ) VALUES ( 426920, '0102011519', 2, '3052181', '0102011519', '2026-08-30', '30364' );INSERT INTO inv_solicitudes_devolucion_d ( documento, codigo_producto, cantidad, documentos_despachos, codigo_producto_sol, fecha_vencimiento, lote ) VALUES ( 426920, '0102030989', 1, '3049785', '0102030989', '2026-08-30', '86782' );  
+
+
+-1: ERROR: LA CANTIDAD A DEVOLVER DEL PRODUCTO ES MAYOR QUE LA REGISTRADA EN LA BODEGA DEL PACIENTE EN EL RESULTADO DE LOS CAMPOS(stock_almacen - cantidad_en_devolucion), PRODUCTO[0102030989], SOLICITADO[0102030989], INGRESO[1996739],PREPARACION[] CONTEXT: funciÃ³n PL/pgSQL bodega_paciente_control_bodegas() en la lÃ­nea 670 en RAISE
+
+
+1241
+
+--------------------------------------
+EL VALOR A FACTURAR DE LA CUENTA EXCEDE EL VALOR DEL CONTRATO
+FACTURAR CUENTA 2042293
+ext 2224
+johana otalvaro
+
+---------------------------------------
+ext 1238 
+NEUROPSICOLOGIA cancelacion
+1109560497
+estas citas ya fueron realizadas en el sistema,
+---------------------------------------
+no le permite poner elpaquete de derivaciones en la 
+paciente josleidy 
+1061216482
+ext 1019
+
+
+426936
+---------------------------------------
+no les deja aprobar devoluciones
+192.168.4.6
